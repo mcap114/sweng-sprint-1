67f2e08c614172ea5951d0fa7bf52d3d86dac530
mcap114 <132965794+mcap114@users.noreply.github.com>
2024-05-21 14:03:08 +0800

    initial commit

---
+++ src/main/java/com/s11group2/profiling_database/Profiling_database.java
@@ -0,0 +1,12 @@
+package com.s11group2.profiling_database;
+
+/**
+ *
+ * @author miguel
+ */
+public class Profiling_database {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
