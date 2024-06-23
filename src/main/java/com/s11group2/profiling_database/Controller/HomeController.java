a8794992d13682dd6cc104714ce1e53e6dd43d7f
mcap114 <132965794+mcap114@users.noreply.github.com>
2024-06-23 19:01:50 +0800

    updated home page mapping in HomeController

--- src/main/java/com/s11group2/profiling_database/Controller/HomeController.java
+++ src/main/java/com/s11group2/profiling_database/Controller/HomeController.java
@@ -1,15 +1,13 @@
 package com.s11group2.profiling_database.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model){
-        //model.addAttribute("message", "test home page");
-        return "home";
+    public String home() {
+        return "redirect:/index.html";
     }
 }
