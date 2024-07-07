a416a8d5493b3f645120a7954b9922b19d89b880
azeliahmartinez <azeliah_martinez@dlsu.edu.ph>
2024-07-07 20:59:05 +0800

    filter options now displaying

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,19 +5,32 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-function toggleDropdown() {
-    document.getElementById("dropdown-menu").classList.toggle("show");
-  }
-  
-  window.onclick = function(event) {
-    if (!event.target.matches('.filter-button') && !event.target.matches('.filter-icon') && !event.target.matches('.filter-text')) {
+function toggleDropdownSort() {
+  document.getElementById("dropdown-menu").classList.toggle("show");
+}
+
+function toggleDropdownFilter() {
+  document.getElementById("dropdown-menu-filter2").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.filter-button') && !event.target.matches('.filter-icon') && !event.target.matches('.filter-text')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+          }
       }
-    }
   }
-  
+  if (!event.target.matches('.filter2-button') && !event.target.matches('.filter2-icon') && !event.target.matches('.filter2-text')) {
+      var dropdowns2 = document.getElementsByClassName("dropdown-content-filter2");
+      for (var i = 0; i < dropdowns2.length; i++) {
+          var openDropdown2 = dropdowns2[i];
+          if (openDropdown2.classList.contains('show')) {
+              openDropdown2.classList.remove('show');
+          }
+      }
+  }
+}
+
